e89d2b90d2471075b569d48b6dc31a4a9bccd7ee
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2023-11-14 06:29:22 -0800

    commit

--- main.c
+++ main.c
@@ -38,7 +38,5 @@ len = _printf("Let's try to printf a simple sentence.\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
     return (0);
 }
-
