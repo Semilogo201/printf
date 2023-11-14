e3805c4c7e46a1fe612c4f2cbb3a71fbb6867da0
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2023-11-14 06:49:24 -0800

    commit

--- param.c
+++ param.c
@@ -9,18 +9,15 @@
  */
 void init_param(param_t *param, va_list ap)
 {
-        param->unsign = 0;
-
-        param->plus_flag = 0;
-        param->space_flag = 0;
-        param->hashtag_flag = 0;
-        param->zero_flag = 0;
-        param->minus_flag = 0;
-
-        param->width = 0;
-        param->precision = UINT_MAX;
-
-        param->h_modifier = 0;
-        param->l_modifier = 0;
-        (void)ap;
+	param->unsign = 0;
+	param->plus_flag = 0;
+	param->space_flag = 0;
+	param->hashtag_flag = 0;
+	param->zero_flag = 0;
+	param->minus_flag = 0;
+	param->width = 0;
+	param->precision = UINT_MAX;
+	param->h_modifier = 0;
+	param->l_modifier = 0;
+	(void)ap;
 }
